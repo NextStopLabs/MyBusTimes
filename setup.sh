d2b20639b1fae93bc919d3f325ed51edf96cbd35
Kai <Kaiboucher2007@gmail.com>
2025-04-04 11:20:42 +0100

    Update setup.sh
    Updated to python3

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 set -e
-python -m venv env
+python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install django djangorestframework django-cors-headers djangorestframework-simplejwt django-filter
-python manage.py makemigrations mybustimes
-python manage.py migrate
+python3 manage.py makemigrations mybustimes
+python3 manage.py migrate
 bash runAPI.sh
