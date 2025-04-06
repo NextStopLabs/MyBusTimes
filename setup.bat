0ced3c07c2e3a8ba591db9f6aec17c60e630709a
Kai <Kaiboucher2007@gmail.com>
2025-04-06 21:58:59 +0100

    add npm install

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ python -m venv env
 call env\Scripts\activate
 pip install --upgrade pip
 pip install django djangorestframework django-cors-headers djangorestframework-simplejwt django-filter Pillow
+npm install
 python mybustimesAPI/manage.py makemigrations mybustimes
 python mybustimesAPI/manage.py migrate
 call runAPI.bat
