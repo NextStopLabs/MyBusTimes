8df0ce09d623587a89c090e861e2941e4b8040b3
Kai <Kaiboucher2007@gmail.com>
2025-04-06 21:58:59 +0100

    add npm install

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install django djangorestframework django-cors-headers djangorestframework-simplejwt django-filter Pillow
+npm install
 python3 mybustimesAPI/manage.py makemigrations mybustimes
 python3 mybustimesAPI/manage.py migrate
 bash runAPI.sh
