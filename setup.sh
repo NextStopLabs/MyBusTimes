c63f7cabb44e00e0de013dbf8e5a5a406514f137
Kai <Kaiboucher2007@gmail.com>
2025-04-06 21:38:51 +0100

    added  Pillow to setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ set -e
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
-pip install django djangorestframework django-cors-headers djangorestframework-simplejwt django-filter
+pip install django djangorestframework django-cors-headers djangorestframework-simplejwt django-filter Pillow
 python3 mybustimesAPI/manage.py makemigrations mybustimes
 python3 mybustimesAPI/manage.py migrate
 bash runAPI.sh
