7dde50f88df1940c4455cfb27fe0f5b20f067a27
Kai <Kaiboucher2007@gmail.com>
2025-04-09 16:09:44 +0100

    v2.1.1.0

--- mybustimesAPI/mybustimes/models.py
+++ mybustimesAPI/mybustimes/models.py
@@ -85,6 +85,14 @@ class region(models.Model):
     def __str__(self):
         return self.region_name
 
+class featureToggle(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {'Enabled' if self.enabled else 'Disabled'}"
+
+
 class operator(models.Model):
     id = models.AutoField(primary_key=True)
     operator_name = models.CharField(max_length=50, blank=False)
@@ -218,6 +226,7 @@ class theme(models.Model):
     id = models.AutoField(primary_key=True)
     theme_name = models.CharField(max_length=50, blank=True, null=True)
     css = models.TextField()  # Stores main CSS styles
+    main_colour = models.CharField(max_length=50, blank=True)
     dark_theme = models.BooleanField(default=False)  # Boolean for dark mode
 
     def __str__(self):
--- public/js/ad-script.js
+++ public/js/ad-script.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".ad-box").forEach(adContainer => {
-        // 50% chance to show Google Ad or Custom Ad
-        //const showGoogleAd = Math.random() < 0.5;
+    if (featureToggles['Ads']) {
+        document.querySelectorAll(".ad-box").forEach(adContainer => {
+            // 50% chance to show Google Ad or Custom Ad
+            //const showGoogleAd = Math.random() < 0.5;
 
-        //if (showGoogleAd) {
-        //    // Google Ad
-        //    adContainer.innerHTML = `
-        //        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2623095708353843" crossorigin="anonymous"><\/script>
-        //        <ins class="adsbygoogle" style="display:block" data-adtest="on" data-ad-format="autorelaxed"
-        //            data-ad-client="ca-pub-2623095708353843" data-ad-slot="8420014093"></ins>
-        //        <script>(adsbygoogle = window.adsbygoogle || []).push({});<\/script>
-        //    `;
-        //} else {
-        // Custom Ads with links
+            //if (showGoogleAd) {
+            //    // Google Ad
+            //    adContainer.innerHTML = `
+            //        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2623095708353843" crossorigin="anonymous"><\/script>
+            //        <ins class="adsbygoogle" style="display:block" data-adtest="on" data-ad-format="autorelaxed"
+            //            data-ad-client="ca-pub-2623095708353843" data-ad-slot="8420014093"></ins>
+            //        <script>(adsbygoogle = window.adsbygoogle || []).push({});<\/script>
+            //    `;
+            //} else {
+            // Custom Ads with links
             let adImages = []; // Declare the adImages variable outside the function
 
             async function fetchAds() {
@@ -32,10 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 // Update the ad container with the ad image and link
                 adContainer.innerHTML = `
-            <a href="${randomAd.ad_link}" target="_blank">
-                <img src="${randomAd.ad_img}" alt="Advertisement" style="width:100%;">
-            </a>
-        `;
+                    <a href="${randomAd.ad_link}" target="_blank">
+                        <img src="${randomAd.ad_img}" alt="Advertisement" style="width:100%;">
+                    </a>
+                `;
             }
 
             // Fetch the ad data once before starting the ad rotation
@@ -45,6 +46,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 // Rotate every 15 seconds
                 setInterval(rotateAd, 15000);
             });
-        //}
-    });
+            //}
+        });
+    } else {
+        document.querySelectorAll(".ad-box").forEach(adContainer => {
+            adContainer.style.display = 'none';
+        });
+    }
 });
