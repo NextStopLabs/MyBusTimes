d3ddac590a0b74a31038fff6a48a8b2905f2af43
Kai <Kaiboucher2007@gmail.com>
2025-04-10 11:20:53 +0100

    Update dbupdate.bat

--- dbupdate.bat
+++ dbupdate.bat
@@ -2,3 +2,4 @@
 call env\Scripts\activate
 python mybustimesAPI/manage.py makemigrations mybustimes
 python mybustimesAPI/manage.py migrate
+python mybustimesAPI/manage.py loaddata fixtures/initial_data.json
