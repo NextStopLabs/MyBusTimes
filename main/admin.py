2d14af594ca57f9d977e6ea619562d63f0a3058b
Kai <Kaiboucher2007@gmail.com>
2025-09-06 04:08:37 +0100

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -48,7 +48,15 @@ class CustomUserAdmin(UserAdmin):
             )
         }),
         ('Ban Info', {
-            'fields': ('wiki_edit_banned', 'messaging_banned', 'ticket_banned', 'banned_reason', 'banned_date')
+            'fields': (
+                'banned',
+                'forum_banned',
+                'wiki_edit_banned', 
+                'messaging_banned', 
+                'ticket_banned', 
+                'banned_reason', 
+                'banned_date'
+            )
         }),
         ('Permissions', {
             'fields': (
