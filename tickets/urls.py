312f2a548dd369382f8b83880e8240b106fdf8a9
Kai <Kaiboucher2007@gmail.com>
2025-09-10 06:10:43 +0100

    Update urls.py

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+from django_ratelimit.decorators import ratelimit
+
 urlpatterns = [
     path("", views.ticket_home, name="ticket_home"),
-    path("create/", views.create_ticket, name="create_ticket"),
+    path("create/", ratelimit(key='ip', method='GET', rate='1/m')(views.create_ticket), name="create_ticket"),
     path("<int:ticket_id>/", views.ticket_detail, name="ticket_detail"),  # detail view placeholder
     path("<int:ticket_id>/close/", views.close_ticket, name="ticket_close"),  # close ticket view
     path('banned/', views.ticket_banned, name='ticket_banned'),
