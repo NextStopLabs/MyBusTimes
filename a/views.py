1f0ce61d4a29d41dad9fafbc98bec9a6d156059a
Kai <Kaiboucher2007@gmail.com>
2025-09-21 11:44:57 +0100

    updates

--- a/views.py
+++ a/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Link, AffiliateLink
 from main.models import CustomUser as User
+from django.db.models import Sum
+from django.db.models import F
 
 # Create your views here.
 def a(request, name):
@@ -22,16 +24,39 @@ def affiliate_link(request, name):
             return render(request, 'error/404.html', status=404)
         link = AffiliateLink.objects.create(tag=name, user=user)
 
-    link.clicks += 1
-    link.save()
+    # atomic increment
+    AffiliateLink.objects.filter(id=link.id).update(clicks=F('clicks') + 1)
+    link.refresh_from_db()
 
     response = redirect("/u/register/")
-
     response.set_cookie(
         key="invite_id",
-        value=link.id,                    # store the ID, not the whole object
-        max_age=60 * 60 * 24 * 30,        # 30 days
-        httponly=True,                    # protect from JS
-        secure=True                       # only over HTTPS
+        value=link.id,
+        max_age=60 * 60 * 24 * 30,  # 30 days
+        httponly=True,
+        secure=True
     )
     return response
+
+def your_link(request):
+    if not request.user.is_authenticated:
+        return redirect('/u/login/?next=/invite/your-link/')
+    
+    user = request.user
+    user_link = "/invite/" + user.username
+    
+    current_invites = AffiliateLink.objects.filter(user=user).aggregate(
+        total_clicks=Sum("clicks")
+    )["total_clicks"] or 0
+
+    signups = AffiliateLink.objects.filter(user=user).aggregate(
+        total_signups=Sum("signups_from_clicks")
+    )["total_signups"] or 0
+
+    context = {
+        'user_link': user_link,
+        'current_invites': current_invites,
+        'signups': signups,
+    }
+
+    return render(request, 'your_link.html', context)
--- from/urls.py
+++ from/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from a import views
 
 urlpatterns = [
+    path('your_link/', views.your_link, name='your_link'),
     path('<str:name>', views.affiliate_link, name='affiliate_link'),
 ]
