5bddabe03ee44200a6e7f58765e8067da9bf4cfc
Kai <Kaiboucher2007@gmail.com>
2025-10-15 06:45:59 +0100

    ticket meta data

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,7 +5,7 @@ from django_ratelimit.decorators import ratelimit
 
 urlpatterns = [
     path("", views.ticket_home, name="ticket_home"),
-    path("create/", ratelimit(key='ip', method='GET', rate='1/m')(views.create_ticket), name="create_ticket"),
+    path("create/", ratelimit(key='ip', method='GET', rate='1/s')(views.create_ticket), name="create_ticket"),
     path("<int:ticket_id>/", views.ticket_detail, name="ticket_detail"),  # detail view placeholder
     path("<int:ticket_id>/close/", views.close_ticket, name="ticket_close"),  # close ticket view
     path('banned/', views.ticket_banned, name='ticket_banned'),
