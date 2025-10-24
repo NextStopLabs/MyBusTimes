54b7ad47d2631a4df46e954581b6bf0e155185d0
Kai <Kaiboucher2007@gmail.com>
2025-10-24 22:26:14 +0100

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -36,7 +36,7 @@ class ThemeAdmin(SimpleHistoryAdmin):
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'discord_username', 'join_date', 'banned', 'ad_free_until')
+    list_display = ('username', 'email', 'discord_username', 'join_date', 'banned', 'ad_free_until', 'last_active')
     list_filter = ('banned', 'is_staff', 'is_superuser', 'ad_free_until', 'theme')
     search_fields = ('username', 'email')
     filter_horizontal = ('badges', 'groups', 'user_permissions')
