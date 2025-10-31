7e4c9bcc6faec4e101fc512bb45232d960b88264
Brooklynbull321 <brook1988@outlook.com>
2025-10-31 06:26:22 +0000

    Update views.py

--- fleet/views.py
+++ fleet/views.py
@@ -3128,7 +3128,16 @@ def vehicle_mass_edit(request, operator_slug):
                             {"name": "Type", "value": getattr(vehicle.vehicleType, 'type_name', 'N/A'), "inline": False},
                             {"name": "View", "value": f"https://www.mybustimes.cc/operator/{encoded_operator_slug}/vehicles/{vehicle.id}/?v={random.randint(1000,9999)}", "inline": False}
                         ]
-                        send_discord_webhook_embed(title, description, color=0xFFA500, fields=fields, image_url=f"https://www.mybustimes.cc/operator/vehicle_image/{vehicle.id}/?v={random.randint(1000,9999)}")  # Orange
+                        send_discord_webhook_embed(
+                            title=title,
+                            description=description,
+                            color=0xFFA500,
+                            fields=fields,
+                            image_url=f"https://www.mybustimes.cc/operator/vehicle_image/{vehicle.id}/?v={random.randint(1000,9999)}",
+                            content=content,
+                            allowed_mentions=allowed_mentions
+                        )           
+
                         
                         vehicle.save()
 
